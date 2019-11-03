6797d390a2288b2b4f43d1c02574eaa01e16bca3
David Ikenna <davidikenna271@gmail.com>
2019-11-03 18:39:16 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,165 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Samsung Galaxy A10",
+    img: "https://media.4rgos.it/s/Argos/8877369_R_SET?$Main768$&w=620&h=620",
+    price: 110,
+    company: "Samsung",
+    info: `Samsung Galaxy A10 is powered by an octa-core Exynos 7884 processor. It comes with 2GB of RAM. The Samsung Galaxy A10 runs Android Pie and is powered by a 3400mAh battery. As far as the cameras are concerned, the Samsung Galaxy A10 on the rear packs a 13-megapixel camera with an f/1.9 aperture. It sports a 5-megapixel camera on the front for selfies, with an f/2.0 aperture. The Samsung Galaxy A10 runs One UI based on Android Pie and packs 32GB of inbuilt storage that can be expanded via microSD card (up to 512GB).`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Infinix Hot 8",
+    img:
+      "https://i0.wp.com/techweez.com/wp-content/uploads/2019/10/infinix-Hot-8.jpg?fit=600%2C600&ssl=1",
+    price: 100,
+    company: "Infinix",
+    info:
+      "The Infinix Hot 8 comes with a 6.52 inch display and boasts of an 2.0 GHz Quad Core Cortex A53 (2 GB RAM) /  2.0 GHz Octa Core Cortex A53 (4 GB RAM) Processor with Android 9.0, Pie. It also comes with a 2 GB / 4 GB of RAM, an internal memory of 32 GB / 64 GB. It has a back / rear camera of 13 MP + 2 MP and an 8 MP front camera with a 5000 mah Li-Po battery capacity to push all this.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Tecno Spark 4",
+    img:
+      "https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/K/V/67157_1569926404.jpg",
+    price: 97,
+    company: "Tecno",
+    info:
+      "The Tecno Spark 4 is powered by a 2.0 GHz Quad-Core processor. This is the Mediatek Helio A22 SoC which come loaded with Artificial Intelligence (AI) features. Along with the CPU, there’s a Power VR GE8320 GPU, a 3GB of RAM and 32GB internal storage, expandable up to 128GB via microSD card. It even come with a single front-facing LED flash just above the screen. The screen itself is a 6.1-inch Full View display with 720 x 1560 pixel resolution.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Huawei Y9 Prime",
+    img:
+      "https://gigahertz.com.ph/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/h/u/huawei-y9-prime-2019.jpg",
+    price: 221,
+    company: "Huawei",
+    info:
+      "Huawei Y9 2019 smartphone runs on Android v8.1 (Oreo) operating system. The phone is powered by Octa core (2.2 GHz, Quad core, Cortex A73 + 1.7 GHz, Quad core, Cortex A53) processor. It has 4 GB RAM and 64 GB internal storage. It measures 162.4 mm x 77.1 mm x 8.0 mm and weighs 173 grams. The screen has a resolution of 1080 x 2340 pixels. It has an aspect ratio of 19.5:9 and screen-to-body ratio of 82.65 %. On camera front, the buyers get a 16 MP + 2 MP Dual Front Cameras and on the rear, there's a 13 MP + 2 MP camera.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Gionee A1",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71kKgAK%2B7NL._SL1500_.jpg",
+    price: 330,
+    company: "Gionee",
+    info:
+      "Gionee A1 smartphone has an IPS LCD display. It measures 154.5 mm x 76.5 mm x 8.5 mm and weighs 182 grams. The screen has a resolution of Full HD (1080 x 1920 pixels) and 401 ppi pixel density. On camera front, the buyers get a 16 MP Front Camera and on the rear, there's a 13MP camera with features like Fixed Focus. It is backed by a 4010 mAh battery. Connectivity features in the smartphone include Wi-Fi, Bluetooth, GPS, Volte, and more.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Itel A46",
+    img: "https://www.venfinder.com/upload/P33%20Plus.png",
+    price: 80,
+    company: "Itel",
+    info:
+      "The claim to fame of itel A46 is its 2350 mAh huge battery. You will scarcely ever find such an incredible battery in this value run. The phone has 2 GB RAM and 16 GB internal storage which can be later expanded with the help of microSD, microSDHC, and microSDXC. The smartphone comes with 5.45 inches that gives a resolution of 720 x 1440 pixels. Execution is sensible and Android 9.0 Pie will additionally upgrade it. Taking a camera into consideration, additionally, the double 5 MP self-adjust back camera is likewise really useful at the cost. By and large, if 4G isn’t an issue, this cell phone can at present be a fine decision for you.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Nokia 9",
+    img:
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6325/6325704cv11d.jpg",
+    price: 608,
+    company: "Nokia",
+    info:
+      "Nokia 9 has 5.99-inch, P-OLED capacitive touchscreen, 1440 X 2880 pixels (534 ppi), Android 9.0 Pie, AndroidOne, Octa-core Qualcomm SDM845 Snapdragon 845 with 6GB RAM, 128GB built-in storage with no support for memory card, Penta 12MP camera and a 20MP front camera, Qi Wireless Charging, Fingerprint sensor (under display), 3, 320 mAh non-removable Li-Po battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Oppo Reno",
+    img: "https://www.mobiledokan.co/wp-content/uploads/2019/03/Oppo-Reno.jpg",
+    price: 455,
+    company: "Oppo",
+    info:
+      "Oppo Reno has a height of 15.66cm/156.6mm and width of 7.43cm/9.0mm. It weighs about 185g and has a battery capacity of 3765mAh(TYP), 3680(MIN). It has a RAM of 6GB/8GB and storage capacity of 128GB/256GB. It has a screen size of 6.4” and screen ratio of 93.1%. It has a front sensor of 16MP and rear sensor of 48MP & 5MP.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Samsung Galaxy A30",
+    img:
+      "https://www.bigw.com.au/medias/sys_master/images/images/h9d/hb2/13583844737054.jpg",
+    price: 190,
+    company: "Samsung",
+    info:
+      "Samsung Galaxy A30 has 6.4-inch, Super AMOLED Infinity-V Display, 1080 X 2340 pixels (403 ppi), Android 9.0 Pie, Octa-core Exynos 7904 CPU with 3GB / 4GB RAM 32GB / 64GB built-in storage, up to 512GB memory card, 16MP + 5MP dual rear camera and a 16MP front camera. It also has other features such as Face Unlock, Fast Charging (up to 15W), 4G LTE (up to 150 Mbps), and 4000 mAh Non-removable Li-Po battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Samsung Galaxy A70",
+    img:
+      "https://csmobiles.com/11161-large_default/samsung-galaxy-a70-128gb-4g-dual-sim-black.jpg",
+    price: 310,
+    company: "Samsung",
+    info:
+      "Samsung Galaxy A70 has 6.7-inch Super AMOLED Infinity-U Display, 1080 x 2400 pixels (393 ppi), Android 9 Pie One UI, Qualcomm SDM675 Snapdragon 675 Octa-core (2 x 2.0 GHz, 6 x 1.7 GHz), 6GB RAM / 8GB RAM, 64GB / 128GB Built-in Storage (expandable to 512GB, 32MP Front Camera, 32MP + 8MP + 5MP Triple Rear Camera, Under-screen Fingerprint Sensor, 25W Fast Charging, 4500 mAh Li-Po Non-Removable Battery",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "Infinix S4",
+    img: "https://www.dsclng.com/wp-content/uploads/2019/05/S4.jpg",
+    price: 135,
+    company: "Infinix",
+    info:
+      "The phone comes with a 6.21-inch touchscreen display with a resolution of 720x1520 pixels and an aspect ratio of 19.5:9. Infinix S4 is powered by a 2.2GHz octa-core MediaTek Helio P22 processor. It comes with 3GB of RAM. The Infinix S4 runs Android Pie and is powered by a 4000mAh battery. As far as the cameras are concerned, the Infinix S4 on the rear packs a 13-megapixel primary camera with an f/1.8 aperture; a second 8-megapixel camera with an f/2.2 aperture and a third 2-megapixel camera. It sports a 32-megapixel camera on the front for selfies, with an f/2.0 aperture.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 12,
+    title: "Tecno Pouvoir 3",
+    img: "https://www.droidafrica.net/wp-content/uploads/2019/01/3-600x600.png",
+    price: 110,
+    company: "Tecno",
+    info:
+      "The device is the direct upgrade to the Pouvoir 2. It comes with a large 5000mAh battery in a 6.2 inch display. Under the hood, there is a quad-core Mediatek MT6739 CPU clocked at 1.28 GHz, along with PowerVR GE8100 GPU, 2GB of RAM and a 32GB expandable storage. On the camera department, the Tecno Pouvoir 3 comes with a single 13-megapixel main camera along with dual-toned LED flash, and yet another 13-megapixel front-facing camera, with a single LED flash.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Samsung Galaxy A10",
+  img: "https://media.4rgos.it/s/Argos/8877369_R_SET?$Main768$&w=620&h=620",
+  price: 110,
+  company: "Samsung",
+  info:
+    "Samsung Galaxy A10 is powered by an octa-core Exynos 7884 processor. It comes with 2GB of RAM. The Samsung Galaxy A10 runs Android Pie and is powered by a 3400mAh battery. As far as the cameras are concerned, the Samsung Galaxy A10 on the rear packs a 13-megapixel camera with an f/1.9 aperture. It sports a 5-megapixel camera on the front for selfies, with an f/2.0 aperture. The Samsung Galaxy A10 runs One UI based on Android Pie and packs 32GB of inbuilt storage that can be expanded via microSD card (up to 512GB).",
+  inCart: false,
+  count: 0,
+  total: 0
+};
